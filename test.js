32c723657289e56e70b7f0a88b144f332a4ae97b
ioni_dev <ioni_dev@protonmail.com>
2022-11-21 20:24:16 -0300

    change endpoint

--- test.js
+++ test.js
@@ -5,13 +5,13 @@
   btn.onclick =  async () => {
     const objective = document.getElementById('ultra-feedback');
 
-    
+
     console.log('testing function before fetch->', objective.dataset);
     alert("Button was clicked before fetch");
     console.log('testing function->', objective.dataset);
     alert("Button is clicked objective");
     try {
-      const resp = await fetch('localhost:3000/api/test', {
+      const resp = await fetch('https://next-feedback-me.vercel.app/api/test/', {
         method: 'POST',
         headers: {
           'Accept': 'application/json',
