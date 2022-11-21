0e8faeac2d3eebeec6fc06ef78e3300fa0862ae1
ioni_dev <ioni_dev@protonmail.com>
2022-11-21 19:41:46 -0300

    initial

---
+++ test.js
@@ -0,0 +1,47 @@
+(() => {
+
+  let btn = document.createElement("button");
+  btn.innerHTML = "Click Me";
+  btn.onclick =  async () => {
+    const objective = document.getElementById('ultra-feedback');
+    try {
+      const resp = await fetch('localhost:3000/api/test', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({payload: objective.dataset })
+      })
+  
+      const content = await rawResponse.json();
+      console.log('testing function->', objective.dataset);
+      alert("Button is clicked");
+    } catch (error) {
+      
+    }
+  }
+  document.body.appendChild(btn);
+
+})()
+
+
+// export const sendFeedback = async () => {
+//   const objective = document.getElementById('ultra-feedback');
+//   try {
+//     const resp = await fetch('localhost:3000/api/test', {
+//       method: 'POST',
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify({payload: objective.dataset })
+//     })
+
+//     const content = await rawResponse.json();
+//     console.log('testing function->', objective.dataset);
+
+//   } catch (error) {
+    
+//   }
+// }
